d8eefe49df90322cc6301110a2960fc7b06ccf7a
jurilee0 <juri.lee.dev@gmail.com>
2024-12-21 21:58:52 +0900

    temp deploy

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 function ArrowIcon() {
   return (
     <svg
@@ -16,15 +18,19 @@ function ArrowIcon() {
 }
 
 export default function Footer() {
+  const handleAlert = () => {
+    alert("준비중입니다 XD");
+  };
+
   return (
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <li>
           <a
+            onClick={handleAlert}
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="/rss"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">portfolio site</p>
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,11 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Hello, world? <br /> Hello, stranger!
       </h1>
-      <p className="mb-4 whitespace-pre-line">{`안녕하세요. 만나서 반갑습니다.🌷
+      <p className="mb-4 whitespace-pre-line">{`안녕하세요 만나서 반갑습니다 🌷 
+      Next.js에 대해 공부하기 위해 만든 템플릿 기반 블로그입니다. 
+
+      제가 누군지 궁금하시다면 아래 포트폴리오나 깃허브에 들려주세요! 
+      언제나 환영입니다!
       `}</p>
       <div className="my-8">
         <BlogPosts />
